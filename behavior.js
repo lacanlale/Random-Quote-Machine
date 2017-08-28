20880e88ac7c1f013c64f0ddd40b1d029868fca0
Jonathan Lacanlale <gizmo1398@gmail.com>
2017-08-28 07:18:51 -0700

    behavior.js

--- behavior.js
+++ behavior.js
@@ -1,30 +1,18 @@
-  function get_doc(id){
-    const url = 'https://www.googleapis.com/drive/v3/files/'+id+'?alt=media'
-    if(self.fetch){
-    var setHeaders = new Headers();
-    setHeaders.append('Authorization', 'Bearer ' + authToken.access_token);
-    setHeaders.append('Content-Type', mime);
-
-    var setOptions = {
-        method: 'GET',
-        headers: setHeaders
-    };
-    fetch(url,setOptions)
-        .then(response => { if(response.ok){
-        var reader = response.body.getReader();
-        var decoder = new TextDecoder();
-        reader.read().then(function(result){
-            var data = {}
-            data = decoder.decode(result.value, {stream: !result.done});
-            console.log(data);
-    });
-        }
-    else{
-        console.log("Response wast not ok");
-    }
-  })  .catch(error => {
-        console.log("There is an error " + error.message);
-        });
-    }
+function changeQuote() {
+  var quote = "";
+  var quoteList = [
+    "When you reach the end of your rope, tie a knot in it and hang on. - Franklin D. Roosevelt",
+    "But man is not made for defeat. A man can be destroyed but not defeated. - Ernest Hemingway",
+    "You cannot shake hands with a clenched fist. - Indira Gandhi",
+    "Let us sacrifice our today so that our children can have a better tomorrow. - A. P. J. Abdul Kalam",
+    "When something is important enough, you do it even if the odds are not in your favor. - Elon Musk",
+    "Life is too short for long-term grudges. - Elon Musk",
+    "Patience is a virtue, and I'm learning patience. It's a tough lesson. - Elon Musk",
+    "It's really hard to design products by focus groups. A lot of times, people don't know what they want until you show it to them. - Steve Jobs",
+    "Being the richest man in the cemetery doesn't matter to me. Going to bed at night saying we've done something wonderful, that's what matters to me. - Steve Jobs",
+    "I want to put a ding in the universe. - Steve Jobs"
+  ];
+  quote = quoteList[Math.floor(Math.random() * 10)];
+  document.getElementById("quote-leftside").innerHTML = quote;
 }
-//FAMOUS QUOTES ID LIST: 19a5-0wJPg-vzM9XSQJhHY_3jRc5kqJiGyLwUw5G2Gkw
+document.getElementById("quote-leftside").onClick = changeQuote();
